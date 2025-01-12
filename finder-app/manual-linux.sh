063021cd16a0f587a01343cb4bb6e9520ae9c8c0
Oleksii Khovan <ahovan@gmail.com>
2025-01-12 22:41:32 +0100

    compile writer for ARM

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -103,6 +103,9 @@ sudo mknod console -m 666 c 5 1
 cd ..
 
 # TODO: Clean and build the writer utility
+cd $FINDER_APP_DIR
+make clean
+make CROSS_COMPILE=$CROSS_COMPILE
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
